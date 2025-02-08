f505cd17f93bef7884cd4dd20058869a188ddbe7
Tim Moore <tim.moore@cesium.com>
2025-02-08 20:08:12 +0100

    Fix problems turned up by MSVC

--- src/vsgCs/Styling.cpp
+++ src/vsgCs/Styling.cpp
@@ -322,7 +322,7 @@ std::optional<vsg::vec4> parseColorSpec(const std::string_view expr)
     }
     if (*match.second == '\'' || *match.second == '"')
     {
-        const auto* closing = std::find(match.second + 1, expr.end(), *match.second);
+        const auto closing = std::find(match.second + 1, expr.end(), *match.second);
         if (closing == expr.end())
         {
             return {};
@@ -534,6 +534,7 @@ Stylist::PrimitiveStyling Stylist::getStyling(const CesiumGltf::MeshPrimitive *p
     if (!featureAccessor)
     {
         vsg::info("No feature accessor: ", *featureID.attribute);
+        return result;
     }
     auto colorResult = CesiumGltf::createAccessorView(
         *modelBuilder->_model,
