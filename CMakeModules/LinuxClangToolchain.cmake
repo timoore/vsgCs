9a2d32ad0f2b1aca09f2e058f030df1cd5156c04
Tim Moore <tim.moore@cesium.com>
2025-06-09 17:38:18 +0200

    Use Cesium Native v.0.48.0

--- CMakeModules/LinuxClangToolchain.cmake
+++ CMakeModules/LinuxClangToolchain.cmake
@@ -9,5 +9,7 @@ SET(CMAKE_BUILD_WITH_INSTALL_RPATH on)
 # This was borrowed from cesium-unreal, which has this comment:
 #    These were deduced by scouring Unreal's LinuxToolChain.cs.
 # It's not clear if the librarie arguments are necessary or useful.
-SET(CMAKE_CXX_FLAGS "-target x86_64-unknown-linux-gnu")
+SET(CMAKE_CXX_FLAGS "-target x86_64-unknown-linux-gnu -Wno-overriding-option -Wno-#warnings")
+SET(CMAKE_C_FLAGS "-target x86_64-unknown-linux-gnu -Wno-overriding-option -Wno-#warnings")
+
 SET(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld -target x86_64-unknown-linux-gnu -lm -lc -lpthread -lgcc_s -lgcc")
